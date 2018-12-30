e3f34a79c6ffd3f65529cb8b9b6250ec90749285
alexander.norinchak <norinchak@gmail.com>
2018-12-31 00:09:42 +0200

    First version

---
+++ README.md
@@ -0,0 +1,18 @@
+# free-tex-packer-cli
+> Free texture packer CLI 
+
+# Install
+   
+$ npm install -g free-tex-packer-cli
+   
+# Usage
+```
+$ free-tex-packer-cli --project /path/to/project.ftpp --output /path/to/output/folder
+```
+
+# Used libs
+
+* **Free texture packer core** - https://github.com/odrick/free-tex-packer-core
+
+---
+License: MIT
---
+++ index.js
@@ -0,0 +1,127 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const argv = require('optimist').argv;
+const texturePacker = require('free-tex-packer-core');
+
+function isExists(path) {
+    return fs.existsSync(path);
+}
+
+function fixPath(path) {
+    return path.trim().split('\\').join('/');
+}
+
+function getNameFromPath(path) {
+    return path.trim().split('/').pop();
+}
+
+function isFolder(path) {
+    if(isExists(path)) {
+        return fs.statSync(path).isDirectory();
+    }
+    else {
+        path = fixPath(path);
+        let name = getNameFromPath(path);
+        let parts = name.split('.');
+        return parts.length === 1;
+    }
+}
+
+function getFolderFilesList(dir, base = '', list = []) {
+    let files = fs.readdirSync(dir);
+    for(let file of files) {
+        let p = path.resolve(dir, file);
+        if(isFolder(p) && p.toUpperCase().indexOf('__MACOSX') < 0) {
+            list = getFolderFilesList(p, base + file + '/', list);
+        }
+        else {
+            list.push({
+                name: (base ? base : '') + file,
+                path: p
+            });
+        }
+    }
+
+    return list;
+}
+
+function loadImages(images, files) {
+    for(let image of images) {
+        try {
+            files.push({path: image.name, contents: fs.readFileSync(image.path)});
+        }
+        catch(e) {
+        }
+    }
+}
+
+let projectPath = argv.project;
+if(!projectPath) {
+    console.error('Choose project, using --project argument');
+    process.exit();
+}
+
+let outputPath = argv.output;
+if(!outputPath) {
+    outputPath = path.dirname(projectPath);
+}
+
+fs.readFile(projectPath, (err, content) => {
+    if(err) {
+        console.error(err.toString());
+        return;
+    }
+
+    content = content.toString();
+    let project = null;
+    try {
+        project = JSON.parse(content);
+    }
+    catch(e) {
+        console.error('Unsupported project format ' + projectPath);
+        process.exit();
+    }
+
+    let files = [];
+
+    loadImages(project.images, files);
+
+    for(let folder of project.folders) {
+        if(isExists(folder)) {
+            let list = getFolderFilesList(folder, getNameFromPath(folder) + '/');
+            loadImages(list, files);
+        }
+    }
+
+    let options = project.packOptions;
+
+    //Map exporters to core format
+    if(options.exporter === 'JSON (hash)') options.exporter = 'JsonHash';
+    if(options.exporter === 'JSON (array)') options.exporter = 'JsonArray';
+    if(options.exporter === 'XML') options.exporter = 'XML';
+    if(options.exporter === 'css (modern)') options.exporter = 'Css';
+    if(options.exporter === 'css (old)') options.exporter = 'OldCss';
+    if(options.exporter === 'pixi.js') options.exporter = 'Pixi';
+    if(options.exporter === 'Phaser (hash)') options.exporter = 'PhaserHash';
+    if(options.exporter === 'Phaser (array)') options.exporter = 'PhaserArray';
+    if(options.exporter === 'Spine') options.exporter = 'Spine';
+    if(options.exporter === 'cocos2d') options.exporter = 'Cocos2d';
+    if(options.exporter === 'UnrealEngine') options.exporter = 'Unreal';
+    if(options.exporter === 'Starling') options.exporter = 'Starling';
+
+    if(options.exporter === 'custom') {
+        console.error('CLI does not support a custom exporter');
+        process.exit();
+    }
+
+    texturePacker(files, options, (files) => {
+        for(let file of files) {
+            fs.writeFileSync(outputPath + '/' + file.name, file.buffer);
+        }
+
+        console.log("Done");
+    });
+});
+
